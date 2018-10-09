b4d94d7475cc8b7d78771a3e6d03050be6c19ec4
hachimi ahmed amine <hachimiaminne@gmail.com>
2018-10-09 20:10:36 +0100

    updating myLibrary

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const data = require('./data.json')
 const uniqueRandomArray = require('unique-random-array')
 module.exports={
   all:data,
-  random:uniqueRandomArray(data)
+  random:uniqueRandomArray(datad)
 }
